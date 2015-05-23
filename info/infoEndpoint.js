1ee3d442be7a367a119249d0e3fe95c219177f58
falbuquerque <albuquerque.felipe@gmail.com>
2015-05-23 10:03:00 -0300

    Alterando a chamada e o retorno do get-info

--- info/infoEndpoint.js
+++ info/infoEndpoint.js
@@ -3,15 +3,10 @@ module.exports = router;
 
 var Client = require('node-rest-client').Client;
 var restClient = new Client();
-restClient.registerMethod('getFromFacebook', 'http://graph.facebook.com/44', 'GET');
 restClient.registerMethod('getFromWeather', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk', 'GET');
 
 router.get('/get-info', function(request, response) {
-  restClient.methods.getFromFacebook(handleJson);
-  restClient.methods.getFromWeather(handleJson);
-  response.status(200).json({'status': 'done'});
+  restClient.methods.getFromWeather(function(data, weatherResponse) {
+    response.status(200).json(JSON.parse(data));
+  });
 });
-
-function handleJson(json, response) {
-  console.log(JSON.parse(json));
-}
